8a96cb8c3313cd1a1d5fd5b04249c05626931d05
mrefish <mrefish@gmail.com>
2015-03-31 18:13:57 -0600

    A bit of cleanup.

--- src/main/java/quux00/wordcount/acking/SimpleBolt.java
+++ src/main/java/quux00/wordcount/acking/SimpleBolt.java
@@ -1,7 +1,6 @@
 package quux00.wordcount.acking;
 
 import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
 import java.util.Map;
 
 import backtype.storm.task.OutputCollector;
@@ -14,16 +13,11 @@ import backtype.storm.tuple.Values;
 
 public class SimpleBolt extends BaseRichBolt {
 
-  // private static final long serialVersionUID = 6843364678084556655L;
-  // private static final Long ONE = Long.valueOf(1);
-
   private OutputCollector collector;
-  // private Map<String, Long> countMap;
 
   @Override @SuppressWarnings("rawtypes")
   public void prepare(Map stormConf, TopologyContext context, OutputCollector outCollector) {
     collector = outCollector;
-    // countMap = new HashMap<>();
   }
 
   @Override
--- src/main/java/quux00/wordcount/kafka/WordCountAckedTopology.java
+++ src/main/java/quux00/wordcount/kafka/WordCountAckedTopology.java
@@ -1,9 +1,6 @@
 package quux00.wordcount.kafka;
 
-import quux00.wordcount.acking.ReportBolt;
 import quux00.wordcount.acking.SimpleBolt;
-import quux00.wordcount.acking.SplitSentenceBolt;
-import quux00.wordcount.acking.WordCountBolt;
 import storm.kafka.KafkaSpout;
 import storm.kafka.SpoutConfig;
 import backtype.storm.Config;
@@ -11,12 +8,10 @@ import storm.kafka.bolt.KafkaBolt;
 import storm.kafka.bolt.selector.DefaultTopicSelector;
 import storm.kafka.bolt.mapper.FieldNameBasedTupleToKafkaMapper;
 import storm.kafka.ZkHosts;
-import backtype.storm.Config;
 import backtype.storm.StormSubmitter;
 import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.tuple.Fields;
 import java.util.Properties;
-import storm.kafka.trident.TridentKafkaState;
+
 
 public class WordCountAckedTopology {
 
@@ -24,9 +19,9 @@ public class WordCountAckedTopology {
   private static final String SIMPLE_BOLT_ID = "acking-simple-bolt";
   private static final String TOPOLOGY_NAME = "acking-word-count-topology";
   private static final String TOPIC = "sentences";
+  private static final int numSpoutExecutors = 1;
 
   public static void main(String[] args) throws Exception {
-    int numSpoutExecutors = 1;
 
     // make kafka spout, simple bolt, and kafka bolt
     KafkaSpout kspout = buildKafkaSentenceSpout();
@@ -35,19 +30,21 @@ public class WordCountAckedTopology {
       .withTopicSelector(new DefaultTopicSelector(TOPIC))
       .withTupleToKafkaMapper(new FieldNameBasedTupleToKafkaMapper("key", "message"));
 
-    Config conf = new Config();
-
-    //set kafka producer properties.
+    // set kafka producer properties.
     Properties props = new Properties();
     props.put("metadata.broker.list", "localhost:9092");
     props.put("request.required.acks", "1");
     props.put("serializer.class", "kafka.serializer.StringEncoder");
-    conf.put(KafkaBolt.KAFKA_BROKER_PROPERTIES, props);
 
+    // make topology
     TopologyBuilder builder = new TopologyBuilder();
     builder.setSpout(SENTENCE_SPOUT_ID, kspout, numSpoutExecutors);
     builder.setBolt(SIMPLE_BOLT_ID, simpleBolt).shuffleGrouping(SENTENCE_SPOUT_ID);
     builder.setBolt("forwardToKafka", bolt, 1).shuffleGrouping(SIMPLE_BOLT_ID);
+
+    // create topology config
+    Config conf = new Config();
+    conf.put(KafkaBolt.KAFKA_BROKER_PROPERTIES, props);
 
     StormSubmitter.submitTopology(TOPOLOGY_NAME, conf, builder.createTopology());
   }
